6c5296a85758dd090fc2b0d32e3aeaa7e8788eda
Erbosyn Nurbol <ErbosynNurbol@gmail.com>
2023-11-10 12:16:50 +0600

    dd

---
+++ js/main.js
@@ -0,0 +1,75 @@
+const getCurrentLang = ()=>{
+    let search = window.location.search.substring(1);//?lang=latyn&ss=22&nur=bo
+    let currentLang = '';
+    if(search){
+        let sArr = search.split('&');//=>['lang=latyn','ss=22','nur=bol']
+        for(let param of sArr){
+            let pArr = param.split('=');//=> ['lang','latyn']
+            if(pArr.length!=2){
+                continue;
+            }
+            let pKey = pArr[0];
+            if(pKey == 'lang'){
+                currentLang = pArr[1];
+                break;
+            }
+        }
+    }
+    return currentLang?currentLang:'kz';
+}
+const replaceSearch = (pK,pV)=>{
+    let search = window.location.search.substring(1);
+    if(!search){
+        return '?'+pK+'='+pV;
+    }
+    let sArr = search.split('&');
+    let newSearch = '';
+    let hasKey = false;
+    for(let param of sArr){
+        let pArr = param.split('=');//=> ['lang','latyn']
+        if(pArr.length!=2){
+            continue;
+        }
+        if(pArr[0].toLowerCase() == pK.toLowerCase()){
+            newSearch += (newSearch?'&':'') + pArr[0]+'='+pV;
+            hasKey = true;
+        }else{
+            newSearch += (newSearch?'&':'') + pArr[0]+'='+ pArr[1];
+        }
+    }
+    if(!hasKey){
+        newSearch += (newSearch?'&':'') + pK+'='+pV;
+    }
+    return "?"+newSearch;
+};
+
+const chageLanguageValue = ()=>{
+    let localArr = document.querySelectorAll('[data-localkey]');
+    localArr.forEach((el)=>{
+       let localKey = el.getAttribute('data-localkey');//ls_Home
+       let currentLang = getCurrentLang();
+       let localValue = window.words[localKey][currentLang];
+       el.innerText = localValue;
+    });
+};
+
+const getWords = async ()=>{
+   let response = await fetch('https://www.sozdikqor.org/api/query/all',{'method':'POST'});
+   window.words = await response.json();
+   chageLanguageValue();
+}
+
+document.addEventListener('DOMContentLoaded',function(){ //Domcument Object Model Content Loaded!
+    const lang = document.querySelector('#languages');
+    let currentLang = getCurrentLang();
+    lang.value = currentLang; 
+
+    lang.addEventListener('change',function(){
+       let selectedValue = this.value;
+       let url = window.location.protocol+"//"+window.location.host;
+        window.location.href = url + window.location.pathname + replaceSearch('lang',selectedValue);
+    });
+
+     getWords();
+
+});
